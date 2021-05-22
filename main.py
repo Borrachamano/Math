3eb68c1afb76ceb53a4549afc75e68c41b1227a4
Dur-hy <76396111+Borrachamano@users.noreply.github.com>
2021-05-22 09:55:08 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+# Calculo de potencia
+
+import potencia
+
+clc = potencia.po(5, 2)
+
+print(clc)
+
+# Consulo de factorial
+
+import fatorial
+
+fr = fatorial.fac(5)
+
+print(fr)
+
+# Converte um numero negativo para possitivo
+
+import pos
+
+n = pos.possitivo(-8)
+
+print(n)
+
+# E tambem converte numeros possitivos para negativos
+
+n = pos.negativo(8)
+
+print(n)
+
+# Calcula a raiz quadrada de um numero
+
+import raizq
+
+r = raizq.rq(25)
+
+print(r)
+
