5fe20c6221ae464d49e69173966157a465c1b6d9
Dur-hy <76396111+Borrachamano@users.noreply.github.com>
2021-05-22 09:49:36 -0400

    Create potencia.py

---
+++ potencia.py
@@ -0,0 +1,15 @@
+ po(base, expoente):
+	result = 1
+	for c in range(0, expoente):
+		result *= base
+	return result
+
+if __name__ == '__main__':
+	try:
+		b = int(input('Base: '))
+		e = int(input('Expoente: '))
+		r = po(b, e)
+		print('Resultado', r)
+	except:
+		print('Error.')
+
